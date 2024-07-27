ba2a1960a5a0ad8ffbc1d087791451da5c242666
lalitkudikala <lalitk1405@gmail.com>
2024-07-27 16:27:00 -0500

    Add legend, paragraph, and update colors for charts

--- scene1.js
+++ scene1.js
@@ -5,8 +5,8 @@ function loadScene1() {
             d.Life_Expectancy = +d.Life_Expectancy;
         });
 
-        var svg = d3.select("#scene-container").append("svg").attr("width", 800).attr("height", 400);
-        var margin = {top: 50, right: 50, bottom: 50, left: 50};
+        var svg = d3.select("#scene-container").append("svg").attr("width", 1000).attr("height", 500); // Increased width to accommodate legend
+        var margin = {top: 50, right: 150, bottom: 50, left: 50}; // Increased right margin for legend
         var width = 800 - margin.left - margin.right;
         var height = 400 - margin.top - margin.bottom;
 
@@ -45,7 +45,32 @@ function loadScene1() {
                 });
         });
 
+        // Add legend
+        var legend = svg.append("g")
+            .attr("transform", `translate(${width + margin.right / 2}, ${margin.top})`);
+
+        countries.forEach((country, i) => {
+            legend.append("rect")
+                .attr("x", 0)
+                .attr("y", i * 20)
+                .attr("width", 10)
+                .attr("height", 10)
+                .attr("fill", color(country));
+
+            legend.append("text")
+                .attr("x", 20)
+                .attr("y", i * 20 + 10)
+                .text(country);
+        });
+
         g.append("text").attr("x", width / 2).attr("y", -10).attr("text-anchor", "middle").attr("class", "annotation").text("Life Expectancy Over Time");
+
+        // Add paragraph explaining trends
+        d3.select("#scene-container").append("div")
+            .attr("class", "paragraph")
+            .style("width", "800px")
+            .style("margin-top", "20px")
+            .html("<p>This chart shows the life expectancy trends over time for various countries. It highlights the differences in life expectancy across countries and how it has changed from 2010 to 2019.</p>");
     });
 
     d3.select("body").append("div").attr("id", "tooltip").style("position", "absolute").style("text-align", "center").style("width", "120px").style("height", "50px").style("padding", "2px")
--- scene2.js
+++ scene2.js
@@ -5,13 +5,15 @@ function loadScene2() {
             d.Expenditure_Percentage_GDP = +d.Expenditure_Percentage_GDP;
         });
 
-        var svg = d3.select("#scene-container").append("svg").attr("width", 800).attr("height", 400);
-        var margin = {top: 50, right: 50, bottom: 50, left: 50};
+        var svg = d3.select("#scene-container").append("svg").attr("width", 1000).attr("height", 500); // Increased width to accommodate legend
+        var margin = {top: 50, right: 150, bottom: 50, left: 50}; // Increased right margin for legend
         var width = 800 - margin.left - margin.right;
         var height = 400 - margin.top - margin.bottom;
 
         var x = d3.scaleBand().domain(data.map(d => d.Country)).range([0, width]).padding(0.1);
         var y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
+
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
 
         var g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
@@ -22,19 +24,50 @@ function loadScene2() {
         g.append("g")
             .call(d3.axisLeft(y));
 
-        g.selectAll(".bar").data(data).enter().append("rect").attr("class", "bar").attr("x", d => x(d.Country)).attr("y", d => y(d.Expenditure_Percentage_GDP))
-            .attr("width", x.bandwidth()).attr("height", d => height - y(d.Expenditure_Percentage_GDP)).attr("fill", "steelblue")
-            .on("mouseover", function(event, d) {
-                d3.select(this).attr("fill", "orange");
-                d3.select("#tooltip").style("left", (event.pageX + 10) + "px").style("top", (event.pageY - 10) + "px").style("display", "inline-block")
-                    .html(`Country: ${d.Country}<br>Expenditure: ${d.Expenditure_Percentage_GDP}%`);
-            })
-            .on("mouseout", function() {
-                d3.select(this).attr("fill", "steelblue");
-                d3.select("#tooltip").style("display", "none");
-            });
+        var nestedData = d3.group(data, d => d.Country);
 
-        g.append("text").attr("x", width / 2).attr("y", -10).attr("text-anchor", "middle").attr("class", "annotation").text("Current Healthcare Expenditure (% of GDP)");
+        nestedData.forEach((values, key) => {
+            var barGroup = g.selectAll(`.bar-${key.replace(/ /g, '-')}`)
+                .data(values)
+                .enter()
+                .append("rect")
+                .attr("class", `bar-${key.replace(/ /g, '-')}`)
+                .attr("x", d => x(d.Country))
+                .attr("y", d => y(d.Expenditure_Percentage_GDP))
+                .attr("width", x.bandwidth() / values.length)
+                .attr("height", d => height - y(d.Expenditure_Percentage_GDP))
+                .attr("fill", color(key))
+                .attr("transform", (d, i) => `translate(${i * (x.bandwidth() / values.length)}, 0)`)
+                .on("mouseover", function(event, d) {
+                    d3.select(this).attr("fill", "orange");
+                    d3.select("#tooltip").style("left", (event.pageX + 10) + "px").style("top", (event.pageY - 10) + "px").style("display", "inline-block")
+                        .html(`Country: ${d.Country}<br>Year: ${d.Year}<br>Expenditure: ${d.Expenditure_Percentage_GDP}%`);
+                })
+                .on("mouseout", function() {
+                    d3.select(this).attr("fill", color(key));
+                    d3.select("#tooltip").style("display", "none");
+                });
+        });
+
+        // Add legend
+        var legend = svg.append("g")
+            .attr("transform", `translate(${width + margin.right / 2}, ${margin.top})`);
+
+        nestedData.forEach((values, key, i) => {
+            legend.append("rect")
+                .attr("x", 0)
+                .attr("y", i * 20)
+                .attr("width", 10)
+                .attr("height", 10)
+                .attr("fill", color(key));
+
+            legend.append("text")
+                .attr("x", 20)
+                .attr("y", i * 20 + 10)
+                .text(key);
+        });
+
+        g.append("text").attr("x", width / 2).attr("y", -10).attr("text-anchor", "middle").attr("class", "annotation").text("Healthcare Expenditure by Country (All Years)");
     });
 
     d3.select("body").append("div").attr("id", "tooltip").style("position", "absolute").style("text-align", "center").style("width", "120px").style("height", "50px").style("padding", "2px")
